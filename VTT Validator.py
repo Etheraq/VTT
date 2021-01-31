055fe985f0a06a808aefa559d0a59ca43f2c9303
Etheraq <aaronrbarnes@gmail.com>
2021-01-31 03:40:35 -0600

    Created and updated Entry Class and Validator Functions

--- VTT Validator.py
+++ VTT Validator.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
-
+"""
+    VTTValidator function to parse through a VTT file and confirm that
+    it is a valid WEBVTT file according to the wc3 specification
+"""
+# TODO Write VTTValidator.py function
